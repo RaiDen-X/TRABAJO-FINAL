621d6dfaa8faa8da9a728425fb56b0d2af7c5b88
RaiDen-X <matiasmar2004@hotmail.com>
2024-06-10 20:22:15 -0300

    Add files via upload

---
+++ Trabajo Final/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int opcion;
+int main()
+{
+    while (1==1) {
+    cout << "Elija el modo de juego: 1 Jugador(1) 2 Jugadores(2) Puntuacion mas alta(3) Salir(4)" << endl;
+    cin >> opcion;
+
+    switch (opcion)
+    {
+case 1:
+    cout << "1 jugador" << endl;
+    break;
+case 2:
+    cout << "2 jugadores" << endl;
+    break;
+case 3:
+    cout << "puntuacion mas alta" << endl;
+    break;
+case 4:
+    exit (EXIT_SUCCESS);
+    break;
+    }}
+
+    return 0;
+}
+
+int Dado
+{
+
+
+}
